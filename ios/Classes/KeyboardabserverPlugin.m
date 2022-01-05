db81f7c9e695ffb9a9a0ee96c95392161be933b4
lijunlin <327603258@qq.com>
2022-01-05 12:28:43 +0800

    null safety

--- ios/Classes/KeyboardabserverPlugin.m
+++ ios/Classes/KeyboardabserverPlugin.m
@@ -2,19 +2,19 @@
 
 @interface KeyboardabserverPlugin ()<FlutterStreamHandler>
 
-//事件的channel
+//channel
 @property(nonatomic,weak) FlutterEventChannel* eventChannel;
 
-//事件的event
+//eventSink
 @property(nonatomic,strong) FlutterEventSink eventSink;
 
 //frameView
 @property(nonatomic,strong) UIView* frameView;
 
-//高度
+//height
 @property(nonatomic,assign) double height;
 
-//宽度
+//width
 @property(nonatomic,assign) double width;
 
 @end
@@ -24,61 +24,57 @@
 
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-    //创建Event
     FlutterMethodChannel* channel = [FlutterMethodChannel
                                      methodChannelWithName:@"keyboardabserver"
                                      binaryMessenger:[registrar messenger]];
     
-    //创建eventChannel
+    //create eventChannel
     FlutterEventChannel* eventChannel=[FlutterEventChannel eventChannelWithName:@"keyboardabserver_event"
                                                                 binaryMessenger:[registrar messenger]];
-    //初始化
+    //init
     KeyboardabserverPlugin* instance = [[KeyboardabserverPlugin alloc] init];
     
-    //赋值eventChannel
+    //set eventChannel
     instance.eventChannel=eventChannel;
     
-    //真实设置Handler
+    //set Handler
     [instance.eventChannel setStreamHandler:instance];
     
-    //注册
+    //set delegate
     [registrar addMethodCallDelegate:instance channel:channel];
 }
 
-//执行
+
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-    //什么其他方法都没有实现
     result(FlutterMethodNotImplemented);
 }
 
-//初始化event
+//init frame view
 -(void)initFrameView{
-    //获取根目录
+    //get root controller
     UIViewController* root=[[UIApplication sharedApplication].keyWindow rootViewController];
-    //拿到最顶层的controller
+    //top controller
     UIViewController *topController = [self _topViewController:root];
-    //高度
+    //height
     _height=topController.view.bounds.size.height;
-    //宽度
+    //widht
     _width=topController.view.bounds.size.width;
-    //位置位于最下方
+    //create frame view
     _frameView  = [[UIView alloc]initWithFrame:CGRectMake(0,
                                                           _height,
                                                           _width,
                                                           0)];
-    //添加
+    //add frame view
     [topController.view addSubview:_frameView];
 }
 
-//释放
+//disposeFrameView
 -(void)disposeFrameView{
-    //移除
     [_frameView removeFromSuperview];
-    //清空
     _frameView=nil;
 }
 
-//这里是获取整个应用的顶部Controller;
+//get top
 - (UIViewController *)_topViewController:(UIViewController *)vc {
     if ([vc isKindOfClass:[UINavigationController class]]) {
         return [self _topViewController:[(UINavigationController *)vc topViewController]];
@@ -90,16 +86,15 @@
     return nil;
 }
 
-//循环
+//stream
 -(void)timerAction:(id)data{
-    //返回高度
     if(_frameView!=nil&&_eventSink!=nil){
         _eventSink([NSNumber numberWithDouble:_height-_frameView.layer.presentationLayer.frame.origin.y]);
     }
 }
 
 
-//监听显示
+//keyboard notifications
 - (void)keyboardWillShowNotification:(NSNotification *)notification
 {
     
@@ -137,7 +132,7 @@
     
 }
 
-//监听隐藏
+//hide keyboard notifications
 - (void)keyboardWillHideNotification:(NSNotification *)notification
 {
     CGRect keyboardEndFrameWindow                         = [KeyboardabserverPlugin returnKeyBoardWindow:notification];
@@ -187,38 +182,27 @@
 
 
 #pragma mark - <FlutterStreamHandler>
-// // 这个onListen是Flutter端开始监听这个channel时的回调，第二个参数 EventSink是用来传数据的载体。
 - (FlutterError* _Nullable)onListenWithArguments:(id _Nullable)arguments
                                        eventSink:(FlutterEventSink)events {
-    // 回调给flutter， 建议使用实例指向，因为该block可以使用多次
     _eventSink=events;
-    //设置初始化
     [self initFrameView];
-    //设置监听
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(keyboardWillShowNotification:) name:UIKeyboardWillShowNotification object:nil];
-    //设置监听
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(keyboardWillHideNotification:) name:UIKeyboardWillHideNotification object:nil];
-    //赋值参数
     return nil;
 }
 
-//flutter不再接收
+
 - (FlutterError* _Nullable)onCancelWithArguments:(id _Nullable)arguments {
-    //清空参数
     _eventSink=nil;
-    //移除监听
     [[NSNotificationCenter defaultCenter] removeObserver:self
                                                     name:UIKeyboardWillShowNotification
                                                   object:nil];
-    //设置监听
     [[NSNotificationCenter defaultCenter] removeObserver:self
                                                     name:UIKeyboardWillHideNotification
                                                   object:nil];
-    //释放frameView
     [self disposeFrameView];
-    //清空参数
     return nil;
 }
 
